bbdd0fca1154769bfcd5a29fcbf81158820e9747
Adrian Ambrus <adrian@smartup.io>
2017-05-17 12:24:27 +0300

    Fixed SQSConnectionFactory configuration (#10)
    - now a region and an endpoint can be configured
    at the same time

--- localstack-spring-boot-autoconfigure/src/main/java/io/smartup/cloud/configurator/SQSConnectionFactoryConfigurator.java
+++ localstack-spring-boot-autoconfigure/src/main/java/io/smartup/cloud/configurator/SQSConnectionFactoryConfigurator.java
@@ -3,6 +3,9 @@ package io.smartup.cloud.configurator;
 import com.amazon.sqs.javamessaging.SQSConnectionFactory;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
+import org.springframework.util.ReflectionUtils;
+
+import java.lang.reflect.Field;
 
 public class SQSConnectionFactoryConfigurator extends AbstractAmazonClientConfigurator<SQSConnectionFactory> {
     @Override
@@ -18,5 +21,23 @@ public class SQSConnectionFactoryConfigurator extends AbstractAmazonClientConfig
     @Override
     public Region getRegion() {
         return Region.getRegion(Regions.DEFAULT_REGION);
+    }
+
+    @Override
+    protected SQSConnectionFactory setup() {
+        SQSConnectionFactory amazonBean = getBean();
+
+        Field region = ReflectionUtils.findField(SQSConnectionFactory.class, "region");
+        Field endpoint = ReflectionUtils.findField(SQSConnectionFactory.class, "endpoint");
+
+        region.setAccessible(true);
+        endpoint.setAccessible(true);
+
+        preProcessBean(amazonBean);
+        ReflectionUtils.setField(region, amazonBean, getRegion());
+        ReflectionUtils.setField(endpoint, amazonBean, getEndpoint());
+        postProcessBean(amazonBean);
+
+        return amazonBean;
     }
 }
