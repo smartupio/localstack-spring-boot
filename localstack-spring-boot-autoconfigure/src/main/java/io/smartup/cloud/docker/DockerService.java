8a6ac99f1d8799166158fd34e4a933e2c6c7f352
Ambrus Adrián-Zoltán <ambrusadrianz@gmail.com>
2017-05-30 14:55:47 +0300

    Removed Progress handler (#1)
    * Fixed NPE when pulling image
    
    * Removed the PrintProgress method

--- localstack-spring-boot-autoconfigure/src/main/java/io/smartup/cloud/docker/DockerService.java
+++ localstack-spring-boot-autoconfigure/src/main/java/io/smartup/cloud/docker/DockerService.java
@@ -6,14 +6,11 @@ import com.spotify.docker.client.messages.Container;
 import com.spotify.docker.client.messages.ContainerConfig;
 import com.spotify.docker.client.messages.ContainerCreation;
 import com.spotify.docker.client.messages.Image;
-import com.spotify.docker.client.messages.ProgressDetail;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Wrapper service for the DockerClient class
@@ -94,15 +91,7 @@ public class DockerService {
      */
     public void pullDockerImage(String image) {
         try {
-            System.out.println("Pulling docker image");
-            dockerClient.pull(image, (progressMessage) -> {
-                ProgressDetail progressDetail = progressMessage.progressDetail();
-                printProgress(
-                        progressDetail.start(),
-                        progressDetail.current(),
-                        progressDetail.total()
-                );
-            });
+            dockerClient.pull(image);
         } catch (DockerException | InterruptedException e) {
             throw new DockerServiceException(e);
         }
@@ -188,32 +177,7 @@ public class DockerService {
             throw new DockerServiceException(e);
         }
     }
-
-    private static void printProgress(long startTime, long total, long current) {
-        long eta = current == 0 ? 0 :
-                (total - current) * (System.currentTimeMillis() - startTime) / current;
-
-        String etaHms = current == 0 ? "N/A" :
-                String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(eta),
-                        TimeUnit.MILLISECONDS.toMinutes(eta) % TimeUnit.HOURS.toMinutes(1),
-                        TimeUnit.MILLISECONDS.toSeconds(eta) % TimeUnit.MINUTES.toSeconds(1));
-
-        StringBuilder string = new StringBuilder(140);
-        int percent = (int) (current * 100 / total);
-        string
-                .append('\r')
-                .append(String.join("", Collections.nCopies(percent == 0 ? 2 : 2 - (int) (Math.log10(percent)), " ")))
-                .append(String.format(" %d%% [", percent))
-                .append(String.join("", Collections.nCopies(percent, "=")))
-                .append('>')
-                .append(String.join("", Collections.nCopies(100 - percent, " ")))
-                .append(']')
-                .append(String.join("", Collections.nCopies((int) (Math.log10(total)) - (int) (Math.log10(current)), " ")))
-                .append(String.format(" %d/%d, ETA: %s", current, total, etaHms));
-
-        System.out.print(string);
-    }
-
+    
     private boolean containsLike(String value, Collection<String> values) {
         return values.stream().anyMatch(v -> v.contains(value));
     }
