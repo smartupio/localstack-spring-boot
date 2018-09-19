cd3b743ff37c42055c12f26e0b9b95a4f5814a75
Ambrus Adrián-Zoltán <ambrusadrianz@gmail.com>
2018-09-19 10:44:28 +0300

    Integrated Lambda and SSM services (#8)

---
+++ localstack-spring-boot-autoconfigure/src/main/java/io/smartup/localstack/configurator/AmazonSSMConfigurator.java
@@ -0,0 +1,23 @@
+package io.smartup.localstack.configurator;
+
+
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagement;
+
+public class AmazonSSMConfigurator extends AbstractAmazonClientConfigurator<AWSSimpleSystemsManagement> {
+    @Override
+    public Class<AWSSimpleSystemsManagement> getAmazonClientClass() {
+        return AWSSimpleSystemsManagement.class;
+    }
+
+    @Override
+    public String getEndpoint() {
+        return getLocalStackHost(4583);
+    }
+
+    @Override
+    public Region getRegion() {
+        return Region.getRegion(Regions.DEFAULT_REGION);
+    }
+}
